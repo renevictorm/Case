0cb0e2ea6d9b3318cf3e7797a9eb2142b6ded94c
Renê Milagres <renevictorm@gmail.com>
2018-08-23 18:22:36 -0300

    src/loader.js e configuração do server na porta 8000

---
+++ src/config/server.js
@@ -0,0 +1,14 @@
+const port = 8000
+
+const bodyParser = require('body-parser')
+const express= require ('express')
+const server = express()
+
+
+server.use(bodyParser.urlencoded({extended:true})) // usando o bodyparser para aceitar requisições urlencodede e json 
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+    console.log(`Backend rodando na porta ${port}.`);
+    
+})  
