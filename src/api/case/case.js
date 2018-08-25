a45674e4630e53314961fae1b4bbb02d750cac84
Renê Milagres <renevictorm@gmail.com>
2018-08-25 14:47:26 -0300

    Validação do CNPJ

--- src/api/case/case.js
+++ src/api/case/case.js
@@ -17,4 +17,40 @@ const caseSchema = new mongoose.Schema({
 
 })
 
+caseSchema.path('merchantCnpj').validate(function(merchantCnpj) 
+{   cnpjSplit = merchantCnpj.split("");  
+    //Checando o primeiro dígito
+    
+    weightsOne = [5,4,3,2,9,8,7,6,5,4,3,2 ];
+    checkerOne = 0;
+    digitResultOne = 0;
+    
+    for(i=0; i<12; i++) 
+    {  
+        digitResultOne = digitResultOne +( parseInt(cnpjSplit[i]) * weightsOne[i] )
+    }
+
+    if ((digitResultOne%11)>=2){checkerOne = 11-(digitResultOne%11)}else{checkerOne = 0}
+
+    if ( (checkerOne)!= (parseInt(cnpjSplit[12]))){return false} //se for false não faz sentido calcular o segundo dígito
+    
+    
+    //Checando o segundo dígito
+    weightsTwo = [6,5,4,3,2,9,8,7,6,5,4,3,2 ];
+    checkerTwo = 0;
+    digitResultTwo = 0;
+    
+    for(i=0; i<13; i++) 
+    {  
+        digitResultTwo = digitResultTwo +( parseInt(cnpjSplit[i]) * weightsTwo[i] )
+    }
+    
+    if ((digitResultTwo%11)>=2){checkerTwo = 11-(digitResultTwo%11)}else{checkerTwo = 0}
+
+
+    if ( (checkerTwo) == (parseInt(cnpjSplit[13]))) {  return true }else{return false}
+}, '{PATH} falhou na validação.');
+
+
+
 module.exports = restful.model('Case', caseSchema)
